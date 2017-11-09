9b36c757f75ed68f1ace8b4285520955984730a4
Jordan Posthauer <jlposthau@gmail.com>
2017-11-09 08:18:25 -0600

    Initial submission

--- app/src/main/java/com/qniyaz/mycalc/MainActivity.java
+++ app/src/main/java/com/qniyaz/mycalc/MainActivity.java
@@ -2,6 +2,8 @@ package com.qniyaz.mycalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +18,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void calculate(){
         Button add = (Button) findViewById(R.id.button);
-        Button sub = (Button) findViewById(R.id.button2):
+        Button sub = (Button) findViewById(R.id.button2);
         Button mult = (Button) findViewById(R.id.button3);
         Button div = (Button) findViewById(R.id.button4);
         Button sqrt = (Button) findViewById(R.id.button5);
         Button pow = (Button) findViewById(R.id.button6);
+        EditText result = (EditText) findViewById(R.id.editText3);
+
+        if(add.isActivated())
+        {
+            result.setText("It Works!");
+        }
     }
 
     public void add(){

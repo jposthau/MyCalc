b8766392ea499a08e722ebad98e08a17be73a00c
Jordan Posthauer <jlposthau@gmail.com>
2017-11-08 22:32:54 -0600

    Initial submission

--- app/src/main/java/com/qniyaz/mycalc/MainActivity.java
+++ app/src/main/java/com/qniyaz/mycalc/MainActivity.java
@@ -5,9 +5,36 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public float result;
+    public String op1;
+    public String op2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(){
+
+    }
+
+    public void subtract(){
+
+    }
+
+    public void multiply(){
+
+    }
+
+    public void divide(){
+
+    }
+
+    public void sqrt(){
+
+    }
+
+    public void pow(){
+
     }
 }
